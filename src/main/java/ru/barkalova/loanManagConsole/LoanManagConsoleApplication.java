18a08fc3458f5cd146a94ffa144b634cd4c1210a
beeeekon <barkekvl@mail.ru>
2026-07-01 10:30:19 +0300

    initial commit

---
+++ src/main/java/ru/barkalova/loanManagConsole/LoanManagConsoleApplication.java
@@ -0,0 +1,13 @@
+package ru.barkalova.loanManagConsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanManagConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/barkalova/loanManagConsole/CreditManagConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.barkalova.loanManagConsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
